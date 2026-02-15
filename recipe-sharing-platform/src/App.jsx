8954f85710b527e9f028c31c207375a47f917d54
Just-phareed <aliyufareed29@gmail.com>
2026-02-15 12:25:58 +0200

    Build Home Page with recipe list

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <h1 className="text-blue-500 text-3xl font-bold">
-      Tailwind is working
-    </h1>
-  );
+  return <HomePage />;
 }
 
 export default App;
